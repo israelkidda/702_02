985d893173d0c88bd47b4ac117d7b78ea30679d9
Israel Kidda <israelmarykidda@gmail.com>
2024-10-11 06:39:06 -0400

    initial

---
+++ assignment_02/main.py
@@ -0,0 +1,67 @@
+import textwrap
+
+print("Israel Kidda")
+print("DCP70200/SOC81900/QMSS750")
+print("Methods of Demographic Analysis Fall 2024, CUNY Graduate Center")
+print()
+print("Assignment 2")
+print()
+
+print(textwrap.dedent("""\
+    1. On Blackboard (same folder as this assignment) you will find a spreadsheet with
+    (single-year) cohort age-specific fertility rates for women in the US, Canada, and Spain
+    (Birth Cohorts 1921 and later). Do the following:
+    """))
+print()
+print(textwrap.dedent("""\
+    (a) Calculate the Total Fertility Rate in each country for the following four birth cohorts: 
+        1921, 1936, 1951, 1966 (12 values in total).
+    """))
+print()
+print(textwrap.dedent("""\
+    (b) Graph the age-specific fertility rates of the 1936 birth cohorts. Show the three graphs 
+        (one for each country). Comment on any similarities and differences of cohort age-specific fertility 
+        in the three countries. Start by analyzing the peaks and the degree of dispersion of the 
+        distributions and relate it to differences in fertility timing. (Note that the area underneath each 
+        curve equals the cohort TFR calculated in (a).) When making cross-country comparisons, also think 
+        about the post-WW2 context in each country and recall that we already saw in class that the 1936 US 
+        women were the peak Baby Boom (parent) cohort.
+    """))
+print()
+print(textwrap.dedent("""\
+    (c) For each country separately, prepare a chart of the differences in the cumulative age-specific cohort 
+        fertility rate of the 1921, 1951, and 1966 cohorts relative to the 1936 cohort (make 1936 the 
+        baseline). Comment in detail on the within-country (cross-cohort) evidence regarding timing 
+        differences (including any evidence of fertility postponement, i.e., delay of fertility from younger 
+        to older ages). See slide 15, lecture 5 for an example.
+    """))
+print()
+print(textwrap.dedent("""\
+    2. On Blackboard, you will find an Excel file containing data on the number of deaths and exposure 
+        (person-years) counts by year and sex for the US since 1933. Using Excel (or another program of 
+        your choosing; if you prefer to use another program, please include documentation of your programming), 
+        construct abridged period life tables (follow the US female 2019 example discussed in class) by sex 
+        based on years 2020, 2019, and 1990 data. As in class, when choosing the naₓ values, use Coale-Demeny 
+        model values for very young ages and meaningful approximations (graduation, etc.) for other ages. 
+        Based on your **six** constructed life tables, answer the following questions (in each case begin by 
+        first stating the general expression involving the relevant life table functions (symbols) or 
+        formulas):
+    """))
+print()
+print("(a) What is the female life expectancy at birth?")
+print("(b) What is the male life expectancy at birth?")
+print("(c) What is life expectancy remaining for females at age 65?")
+print("(d) What is life expectancy remaining for males at age 65?")
+print("(e) What fraction of the female life table population survives to age 90?")
+print("(f) What fraction of the male life table population survives to age 80?")
+print("(g) What is the mortality rate of life table females between ages 60 and 100?")
+print("(h) What is the mortality rate of life table males between ages 60 and 100?")
+print("(i) What is the life table probability that a female who reaches age 70 survives to age 90?")
+print("(j) What is the life table probability that a male who reaches age 70 survives to age 90?")
+print("(k) What is the life table probability that a female who survives to age 1 dies between ages 1 and 5?")
+print("(l) What is the number of life table males who die before their fifth birthday?")
+print("(m) What is the life table probability that a newborn male dies between ages 20 and 50?")
+print("(n) What is the life table probability that a female who survives to her 10th birthday dies before she reaches her 40th birthday?")
+print("(o) How many person years are lived in the male life table population between ages 90 and 100?")
+print("(p) On average, how many years can a life table newborn female expect to live between ages 25 and 45?")
+print("(q) What is the average number of years lived by life table male newborns who die before their first birthday?")
